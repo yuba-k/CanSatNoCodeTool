505e852ed5b6bc5e888b2f34ccda6bf55c489388
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-02-07 19:06:46 +0900

    プロジェクトファイルを追加。

---
+++ CanSatNoCodeTool/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CanSatNoCodeTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            await BlocklyView.EnsureCoreWebView2Async();
+            var path = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Blockly",
+                "index.html");
+            Console.WriteLine(path);
+            BlocklyView.Source = new Uri(path);
+        }
+
+    }
+}
